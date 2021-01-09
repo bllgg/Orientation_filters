1ce69fb5468f29fc55a02531fe7ef197e57aa4cf
bllgg <gunathilakebllg@gmail.com>
2021-01-09 08:08:29 +0530

    Madgwick initialized

--- MadgwickAHRS.h
+++ MadgwickAHRS.h
@@ -5,7 +5,16 @@
 #ifndef MADGWICK_TEST_MADGWICKAHRS_H
 #define MADGWICK_TEST_MADGWICKAHRS_H
 
+//----------------------------------------------------------------------------------------------------
+// Variable declaration
 
+extern volatile float beta;				// algorithm gain
+extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
 
+//---------------------------------------------------------------------------------------------------
+// Function declarations
+
+void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
+void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
 
 #endif //MADGWICK_TEST_MADGWICKAHRS_H
