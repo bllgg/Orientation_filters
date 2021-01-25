79cfb2eb2dc0480e7a11db5e02d267580867832b
bllgg <gunathilakebllg@gmail.com>
2021-01-25 23:49:07 +0530

    Added kalman filter

--- kalman.cpp
+++ kalman.cpp
@@ -2,3 +2,70 @@
 // Created by bllgg on 1/19/21.
 //
 
+#include <math.h>
+#include <stdlib.h>
+#include "kalman.h"
+
+/*==============================================================*/
+/*                  Constructor and Destructor                  */
+/*==============================================================*/
+
+Kalman::Kalman() = default;
+
+Kalman::~Kalman() = default;
+
+/*==============================================================*/
+/*                       Public Functions                       */
+/*==============================================================*/
+
+void Kalman::KalmanFilter(double gx, double gy, double gz, double ax, double ay, double az){
+    //prediction step
+    theta_hat_roll = theta_t_minus_1_roll + delta_t * gx;
+    theta_hat_pitch = theta_t_minus_1_pitch + delta_t * gy;
+
+    p_hat_roll += gyro_noise;
+    p_hat_pitch += gyro_noise;
+
+    //measuring step
+    z_k_roll = atan2(-ax, az); // roll angle around x axis
+    z_k_pitch = atan2(-ay, sqrt(ax * ax + az * az)); // pitch angle around y axis
+
+    //Estimation
+    K_roll = p_hat_roll / (p_hat_roll + accel_noise);
+    K_pitch = p_hat_pitch / (p_hat_pitch + accel_noise);
+
+    theta_hat_roll  = theta_hat_roll + K_roll * (z_k_roll - theta_hat_roll);
+    theta_hat_pitch = theta_hat_pitch + K_pitch * (z_k_pitch - theta_hat_pitch);
+
+    p_hat_roll = (1 - K_roll) * p_hat_roll;
+    p_hat_pitch = (1 - K_pitch) * p_hat_pitch;
+
+}
+
+/*==============================================================*/
+/*                      Private Functions                       */
+/*==============================================================*/
+
+double Kalman::radians_to_degrees(double radian_val){
+    double degree_val = radian_val * 180 / M_PI;
+    return degree_val;
+}
+
+double Kalman::degrees_to_radians(double degree_val) {
+    double radian_val = degree_val * M_PI / 180.0;
+    return radian_val;
+}
+
+/*==============================================================*/
+/*                     Getters and Setters                      */
+/*==============================================================*/
+
+double Kalman::get_roll() {
+    roll = radians_to_degrees(theta_hat_roll);
+    return roll;
+}
+
+double Kalman::get_pitch() {
+    pitch = radians_to_degrees(theta_hat_pitch);
+    return pitch;
+}
--- kalman.h
+++ kalman.h
@@ -5,4 +5,62 @@
 #ifndef MADGWICK_TEST_KALMAN_H
 #define MADGWICK_TEST_KALMAN_H
 
+class Kalman{
+public:
+    /*==============================================================*/
+    /*                       Public Variables                       */
+    /*==============================================================*/
+    // no public variables
+
+    /*==============================================================*/
+    /*                  Constructor and Destructor                  */
+    /*==============================================================*/
+    Kalman();
+    ~Kalman();
+
+    /*==============================================================*/
+    /*                       Public Functions                       */
+    /*==============================================================*/
+    //ret val   function name               parameters
+    void        KalmanFilter                (double gx, double gy, double gz, double ax, double ay, double az);
+
+    /*==============================================================*/
+    /*                     Getters and Setters                      */
+    /*==============================================================*/
+    double      get_roll                    (void);
+    void        set_roll                    (double roll);
+
+    double      get_pitch                   (void);
+    void        set_pitch                   (double pitch);
+
+private:
+
+    /*==============================================================*/
+    /*                      Private Variables                       */
+    /*==============================================================*/
+    double      roll = 0;
+    double      pitch = 0;
+    double      gyro_noise = 0;
+    double      accel_noise = 0;
+
+    double      theta_hat_roll = 0;
+    double      theta_hat_pitch = 0;
+    double      theta_t_minus_1_roll = 0;
+    double      theta_t_minus_1_pitch = 0;
+    double      delta_t = 0.01;
+    double      p_hat_roll = 1;
+    double      p_hat_pitch = 1;
+    double      z_k_roll = 0;
+    double      z_k_pitch = 0;
+    double      K_roll = 0;
+    double      K_pitch = 0;
+
+    /*==============================================================*/
+    /*                      Private Functions                       */
+    /*==============================================================*/
+    //ret val   function name               parameters
+    double      radians_to_degrees          (double radian_val);
+    double      degrees_to_radians          (double degree_val);
+};
+
 #endif //MADGWICK_TEST_KALMAN_H
